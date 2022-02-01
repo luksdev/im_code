0f7e24bf79becf1471d24c525a8bf675d6735f0a
luksdev <lucasarinisio@hotmail.com>
2022-02-01 00:23:13 -0300

    version 1.1

--- src/page/main.js
+++ src/page/main.js
@@ -1,8 +1,8 @@
 import NavBar from '../containers/header/navbar';
-import Contact from '../containers/sections/contact/contact';
+// import Contact from '../containers/sections/contact/contact';
 import Home from '../containers/sections/home/home';
 import Project from '../containers/sections/project/project';
-import Team from '../containers/sections/team/team';
+// import Team from '../containers/sections/team/team';
 
 // import GlobalStyle from '../styles/global';
 
